1135127bc6d2592305552374b86900a0ad771b5a
PentBeear <43016309+PentBeear@users.noreply.github.com>
2020-07-13 15:20:42 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,10 +1,10 @@
-class hypixelWrapper {
+class discordWrapper {
     getInfo() {
         return {
-            "id": "HypixelBazaarWrapper",
-            "name": "HypixelBazaarWrapper",
+            "id": "DiscordJsWrapper",
+            "name": "DiscordJsWrapper",
             "blocks": [{
-                "opcode": "jsonGetInfo",
+                "opcode": "loginModule",
                 "blockType": "reporter",
                 "text": "GetBazaarItem: key: [key] product name: [product] select value: [oper]",
                 "arguments": {
@@ -12,88 +12,24 @@ class hypixelWrapper {
                         "type": "string",
                         "defaultValue": "put ya key here"
                     },
-                    "product": {
-                        "type": "string",
-                        "defaultValue": "INK_SACK:3"
-                    },
-                    "oper": {
-                        "type": "string",
-                        "menu": "selectvalue",
-                        "defaultValue": "0"
-                    },
                 }
             }],
-            "menus": {
-                selectvalue: this._formatMenu(['BuyVolume', 'SellVolume', 'BuyPrice', 'SellPrice', 'BuyOrders', 'SellOrders']),
-            }
         };
     }
-   async jsonGetInfo ({key, product, oper}) {
-    console.log(oper);
+   async loginModule ({key, product, oper}) {
     console.log(key);
-    console.log(product);
-    var output = "";
-    var wait = ms => new Promise((r, j)=>setTimeout(r, ms))
-    fetch("https://api.hypixel.net/skyblock/bazaar/product?key=" + key + "&productId=" + product)
-    .then((resp) => resp.json())
-    .then(function(data)
-    {
-        console.log(data)
-        if (oper == 0) {
-            console.log("Getting Buy Volume")
-            output = JSON.stringify(data.product_info.quick_status.buyVolume)   
-            console.log(output);  
- 
-        }
-        if (oper == 1) {
-            console.log("Getting Sell Volume")
-            output = JSON.stringify(data.product_info.quick_status.sellVolume)  
-            console.log(output);  
 
-        }
-        if (oper == 2) {
-            console.log("Getting Buy Price")
-            output = JSON.stringify(data.product_info.quick_status.buyPrice)  
-            console.log(output); 
-     
-        }
-        if (oper == 3) {
-            console.log("Getting Sell Price")
-            output = JSON.stringify(data.product_info.quick_status.sellPrice) 
-            console.log(output);   
-           
-        }
-        if (oper == 4) {
-            console.log("Getting Buy Orders")
-            output = JSON.stringify(data.product_info.quick_status.buyOrders)  
-            console.log(output);   
-        
-        }
-        if (oper == 5) {
-            console.log("Getting Sell Orders")
-            output = JSON.stringify(data.product_info.quick_status.sellOrders)   
-            console.log(output);  
-           
-        }      
-    });
+    var output = "";
+
+    <script type="text/javascript" src="discord.11.1.0.min.js"></script>
+
+    var wait = ms => new Promise((r, j)=>setTimeout(r, ms))
+    
     await wait(1000)
     console.log("Returning value! " + output);
     return output
     };
-
-    _formatMenu(menu) {
-        const m = [];
-        for (let i = 0; i < menu.length; i++) {
-            const obj = {};
-            obj.text = menu[i];
-            obj.value = i.toString();
-            m.push(obj);
-        }
-        return m;
-    }
-   
-
 }
 
     
-Scratch.extensions.register(new hypixelWrapper());
+Scratch.extensions.register(new discordWrapper());
